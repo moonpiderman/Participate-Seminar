0f33ba394bae055017d2000583b26a1ea2184678
moonpiderman <lotsofchuck@gmail.com>
2021-09-08 22:51:22 +0900

    change variable

--- seminar1/assignment1/seminar/src/main/kotlin/com/wafflestudio/seminar/domain/user/api/UserController.kt
+++ seminar1/assignment1/seminar/src/main/kotlin/com/wafflestudio/seminar/domain/user/api/UserController.kt
@@ -18,12 +18,18 @@ class UserController(
     private val userRepository: UserRepository,
     private val modelMapper: ModelMapper
 ) {
+    @GetMapping("/me/")
+    fun userMe(@RequestHeader("User-Id") userId: Long?): UserDto.Response {
+        // 입력받은 id 와 같은 id 가 DB에 있는 데이터와 같은 것의 response를 불러오기.
+//        val userId = userService.getAllUserResponse()
+    }
+
     @PostMapping("/")
     fun createUser(@ModelAttribute @RequestBody @Valid body: UserDto.CreateRequest): UserDto.Response {
 //        user.username = body.username
 //        user.email = body.email
-        val new_user = User(username = body.username, email = body.email)
-        userRepository.save(new_user)
-        return UserDto.Response(id = new_user.id, username = new_user.username, email = new_user.email)
+        val newUser = User(username = body.username, email = body.email)
+        userRepository.save(newUser)
+        return UserDto.Response(id = newUser.id, username = newUser.username, email = newUser.email)
     }
 }
