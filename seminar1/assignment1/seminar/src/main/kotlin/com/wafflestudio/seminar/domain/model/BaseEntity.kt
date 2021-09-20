40b1f4aa7c84b08117939bf18c1e84f839e7f4eb
moonpiderman <lotsofchuck@gmail.com>
2021-09-20 10:35:09 +0900

    base entity

--- seminar1/assignment1/seminar/src/main/kotlin/com/wafflestudio/seminar/domain/model/BaseEntity.kt
+++ seminar1/assignment1/seminar/src/main/kotlin/com/wafflestudio/seminar/domain/model/BaseEntity.kt
@@ -7,8 +7,15 @@ import java.time.LocalDateTime
 import javax.persistence.*
 
 @MappedSuperclass
+//@EntityListeners(AuditingEntityListener::class)
 open class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     open val id: Long = 0
+
+    @CreatedDate
+    open val created_at: LocalDateTime? = null
+
+    @LastModifiedDate
+    open val updated_at: LocalDateTime? = null
 }
